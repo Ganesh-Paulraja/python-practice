59762bcc9b4f56fa69722251827288d2ba422073
GANESH_PAULRAJA <ganeshsgk555@gmail.com>
2025-01-21 15:08:24 +0530

    update

--- higherLower/solution.py
+++ higherLower/solution.py
@@ -1,2 +1,42 @@
 import art
 print(art.logo)
+from data import data
+import random
+
+score = 0
+account_a = random.choice(data)
+account_b = random.choice(data)
+
+if account_a == account_b:
+  account_b = random.choice(data)
+
+
+def formate_data(account):
+  account_name = account["name"]
+  account_descr = account["description"]
+  account_country = account["country"]
+
+  print(f"{account_name} a {account_descr} from {account_country}")
+
+def chedck_answer(user_guess, a_followrs, b_followers):
+  if a_followrs > b_followers:
+    return user_guess == "a"
+  else:
+    return user_guess == "b"
+
+print(f"Compare A: {formate_data(account_a)}")
+print(art.vs)
+print(f"Compare B: {formate_data(account_b)}")
+
+guess = input("Who has more followers? Type 'A' or 'B'")
+
+a_followr_count = account_a["follower_count"]
+b_followr_count = account_b["followr_count"]
+
+is_correct = chedck_answer(guess, a_followr_count, b_followr_count)
+
+if is_correct:
+  score += 1
+  print(f"You're right! Current score {score}")
+else:
+  print("Sorry, that's wrong. Final score: {score}")
--- tipcalc.py
+++ tipcalc.py
@@ -1,4 +1,29 @@
-str = '  Hello  nice'
-list = str.split()
-print(list)
-print(" ".join(list))
+# print("hello " + "World")
+# print("hello" + " " + "world")
+# name = input("What is your name?")
+# print(f"my name is {name}")
+
+print("HaHa " * 3 )
+print("hello".upper())
+print("hello world".title())
+print("hello world".capitalize())
+print('hello world'.find('world'))
+print("Hello world".replace("world", "python"))
+print("helow   ".strip() + 'new')
+print("Hello world".split())
+print(" ".join("Hello World"))
+print(" ".join(["Hello", "world"]))
+print("Hello".isalpha())
+print("Hello".isdigit())
+print("Hello123".isalnum())
+print(len("Hellow"))
+
+text = "Hello world"
+print(text[0:5])
+print(text.count("Hello"))
+print(text[::-1])
+
+
+num = 12345
+
+print(type(num))
