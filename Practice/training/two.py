af5b3d39377a9ec2be0fc79466b178ca9f518adf
GANESH_PAULRAJA <ganeshsgk555@gmail.com>
2025-03-02 11:10:44 +0530

    two mini projects practiced

---
+++ Practice/training/two.py
@@ -0,0 +1,20 @@
+# Loop Structure
+
+# 1. for_loop
+
+# type 1
+
+# for <variable> in range(n): **0 to n-1
+#     statments(s)
+# **0 to n-1
+
+# type 2
+# for <variable> in range(initial, n): **initial to n-1
+#     statments(s)
+# **initial to n-1
+
+# type 3
+# for <variable> in range(initial, n, step): **initial to n-1 , step starts from 2 , -1 for reverse
+#     statments(s)
+# **initial to n-1
+# step starts from 2 , -1 for reverse
